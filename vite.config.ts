c5d8feee0980131f1ebb29edafe0db20e1bfab61
Abdulaziz <me@abdaziz.dev>
2026-08-16 18:20:25 +0300

    fix: Remove nitro
    It breaks SSR and it we don't use it anyways

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import react, {reactCompilerPreset} from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 import tailwindcss from '@tailwindcss/vite'
-import { nitro } from 'nitro/vite'
 
 const config = defineConfig({
   resolve: {
@@ -12,7 +11,6 @@ const config = defineConfig({
   },
   plugins: [
     devtools(),
-    nitro(),
     tailwindcss(),
     tanstackStart(),
     react(),
